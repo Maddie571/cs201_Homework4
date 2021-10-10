feace974eaa89b4725b5c964840405c6417c025e
maddie571 <mattrind2@gmail.com>
2021-10-09 20:19:40 -0800

    the correct getLine is now implemented

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -5,20 +5,15 @@
 #include "tokenizer.hpp"
 #include <iostream>
 #include <vector>
+#include <sstream>
 using std::vector;
 using std::cout;
 using std::endl;
 using std::string;
 using std::cin;
+using std::getline;
+using std::istringstream;
 
-
-string getLine()
-{
-    string s;
-    cout << "Enter a word, or a keyboard smash, enter 'end' to end";
-    cin >> s;
-    return s;
-}
 bool readLine(string & str){
     if (str.size() > 0){
         return true;
@@ -26,5 +21,5 @@ bool readLine(string & str){
     return false;
 }
 unsigned stringToTokensWS(const string & input,vector<string> & tokens){
-
+return 2;
 }
