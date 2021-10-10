94e3a3222dda7a0171ec318a8b86e8fb28e521e5
maddie571 <mattrind2@gmail.com>
2021-10-09 20:02:05 -0800

    getLine and readLine working

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "tokenizer.hpp"
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+
+
+string getLine()
+{
+    string s;
+    cout << "Enter a word, or a keyboard smash, enter 'end' to end";
+    cin >> s;
+    return s;
+}
+bool readLine(string & str){
+    if (str.size() > 0){
+        return true;
+    }
+    return false;
+}
+unsigned stringToTokensWS(const string & input,vector<string> & tokens){
+
+}
--- tokenizertest.cpp
+++ tokenizertest.cpp
@@ -1,4 +1,14 @@
 //
 // Created by Maddie on 10/6/2021.
 //
+#include <iostream>
+#include "tokenizer.cpp"
+using std::cout;
+using std::endl;
 
+int main()
+{
+    bool test = readLine(getLine());
+    cout << test;
+    return 0;
+}
